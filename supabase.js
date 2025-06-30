a58363acc75ea4d3705eeb2d1f50e518ca3c962d
macbook <macbook@MacBook-Air-de-macbook.local>
2025-06-30 00:06:28 -0600

    Fixed server typo

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-export const SUPABASE_URL = 'https://gidpmhcpdrbctknrdzksO.supabase.co';
+export const SUPABASE_URL = 'https://gidpmhcpdrbctknrdzks.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZHBtaGNwZHJiY3RrbnJkemtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNDA1OTQsImV4cCI6MjA2NjgxNjU5NH0.RJgCnkhRmhMd66VEonb8u28i91lKZYLmLa32DdvTmkw';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
