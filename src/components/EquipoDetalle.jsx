6caa20e8257cd7551467b3aa21ddc311d7d976b6
Gael <gael1423@gmail.com>
2026-02-13 14:27:45 -0600

    production

--- src/components/EquipoDetalle.jsx
+++ src/components/EquipoDetalle.jsx
@@ -12,7 +12,7 @@ const DEMO_EQUIPOS_DETALLE = {
   'demo-e1': {
     equipo: {
       id: 'demo-e1',
-      nota: '13500',
+      nota: '123',
       marca: 'Dell',
       modelo: 'Inspiron 15',
       color: 'negro',
@@ -83,7 +83,7 @@ const DEMO_EQUIPOS_DETALLE = {
   'demo-e2': {
     equipo: {
       id: 'demo-e2',
-      nota: '13501',
+      nota: '130',
       marca: 'HP',
       modelo: 'Pavilion 14',
       color: 'gris',
